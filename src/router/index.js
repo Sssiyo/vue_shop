e64e9ba2353377cfb4925d6192e3c8b17948101c
Sssiyo <89689302+Sssiyo@users.noreply.github.com>
2021-10-17 14:52:46 +0800

    完成了权限功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,10 @@ import Login from "../components/Login.vue"
 import Home from "../components/Home.vue"
 import Welcome from "../components/Welcome.vue"
 import Users from "../components/user/Users.vue"
+import Rights from "../components/power/Rights.vue"
+import Roles from "../components/power/Roles.vue"
 import '../assets/css/gloabl.css'
+
 
 
 
@@ -33,10 +36,20 @@ const router = new Router({
         {
           path: '/users',
           component: Users
-        }
+        },
+        {
+          path: '/rights',
+          component: Rights
+        },
+        {
+          path: '/roles',
+          component: Roles
+        },
+
+
       ]
 
-    }
+    },
 
   ]
 })
