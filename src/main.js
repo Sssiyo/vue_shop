3067ffca0ea2dfaa9936534e650c3051092f44ba
Sssiyo <89689302+Sssiyo@users.noreply.github.com>
2021-10-27 11:41:11 +0800

    完成商品功能开发'

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/zh-CN'
 import App from './App'
 import router from './router'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+
 
 
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
@@ -18,9 +26,11 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale })
 
 Vue.component('tree-table', TreeTable)
+
+Vue.use(VueQuillEditor, /* { default global options } */)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from "../components/power/Rights.vue"
 import Roles from "../components/power/Roles.vue"
 import Cate from "../components/goods/Cate.vue"
 import Params from "../components/goods/Params.vue"
+import List from "../components/goods/List.vue"
+import Add from "../components/goods/Add.vue"
 import '../assets/css/gloabl.css'
 
 
@@ -54,7 +56,15 @@ const router = new Router({
         {
           path: '/params',
           component: Params
-        }
+        },
+        {
+          path: '/goods',
+          component: List
+        },
+        {
+          path: '/goods/add',
+          component: Add
+        },
 
 
       ]
